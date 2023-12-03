3a01e57dbaa5868f6986f22a3cb5c1333933bb4e
Maya the bee <15341887+Nurrl@users.noreply.github.com>
2023-12-03 23:44:43 +0100

    Initial setup of egui

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use eframe::egui;
+
+fn main() -> Result<(), eframe::Error> {
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        &format!("glue v{}", env!("CARGO_PKG_VERSION")),
+        options,
+        Box::new(|_| Box::<App>::default()),
+    )
+}
+
+#[derive(Debug, Default)]
+struct App {}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("La glue, ça colle");
+        });
+    }
+}
