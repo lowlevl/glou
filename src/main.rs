d88afdfebc839d30afca75939c3df55697f85e23
Maya the bee <15341887+Nurrl@users.noreply.github.com>
2023-12-04 02:12:54 +0100

    Set up basic program layout

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,77 @@
+use std::collections::BTreeMap;
+
 use eframe::egui;
 
 fn main() -> Result<(), eframe::Error> {
-    let options = eframe::NativeOptions {
-        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
-        ..Default::default()
-    };
-
     eframe::run_native(
         &format!("glou v{}", env!("CARGO_PKG_VERSION")),
-        options,
+        eframe::NativeOptions {
+            viewport: egui::ViewportBuilder {
+                min_inner_size: Some(egui::vec2(320.0, 180.0)),
+                ..Default::default()
+            },
+            centered: true,
+            ..Default::default()
+        },
         Box::new(|_| Box::<App>::default()),
     )
 }
 
 #[derive(Debug, Default)]
-struct App {}
+struct App {
+    path: Option<String>,
+    uniforms: BTreeMap<String, Vec<f32>>,
+}
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("This is an example text");
+        egui::TopBottomPanel::top("bar").show(ctx, |ui| {
+            egui::menu::bar(ui, |ui| {
+                ui.menu_button("File", |ui| {});
+            });
         });
+
+        egui::SidePanel::left("tools").show(ctx, |ui| {
+            ui.vertical_centered(|ui| {
+                ui.label("Currently loaded shader:");
+                ui.monospace(self.path.as_deref().unwrap_or("(none)"));
+
+                ui.collapsing("Uniforms", |ui| {
+                    ui.label("Uniforms value and types provided to the shader.");
+                });
+
+                ui.collapsing("Reference", |ui| {
+                    ui.label("Some documentation about the GLSL methods and types.");
+                });
+            });
+        });
+
+        egui::TopBottomPanel::bottom("errors").show(ctx, |ui| {
+            ui.collapsing("Errors ⚠", |ui| {
+                ui.monospace("There are no errors for now ✔");
+            });
+        });
+
+        egui::CentralPanel::default()
+            .frame(egui::Frame {
+                inner_margin: egui::Margin::ZERO,
+                ..Default::default()
+            })
+            .show(ctx, |ui| {
+                let painter = egui::Painter::new(
+                    ui.ctx().clone(),
+                    ui.layer_id(),
+                    ui.available_rect_before_wrap(),
+                );
+
+                // GLSL goes here
+                painter.rect_filled(
+                    painter.clip_rect(),
+                    eframe::epaint::Rounding::ZERO,
+                    eframe::epaint::Color32::BLACK,
+                );
+
+                ui.expand_to_include_rect(painter.clip_rect());
+            });
     }
 }
