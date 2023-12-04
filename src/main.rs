e4fe84d5bd89b78774305bc364e1bd05b15588b3
Maya the bee <15341887+Nurrl@users.noreply.github.com>
2023-12-04 02:45:42 +0100

    Added a Load shader menu button to top bar

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, path::PathBuf};
 
 use eframe::egui;
 
@@ -19,7 +19,7 @@ fn main() -> Result<(), eframe::Error> {
 
 #[derive(Debug, Default)]
 struct App {
-    path: Option<String>,
+    path: Option<PathBuf>,
     uniforms: BTreeMap<String, Vec<f32>>,
     live_mode: bool,
 }
@@ -33,14 +33,35 @@ impl eframe::App for App {
         if !self.live_mode {
             egui::TopBottomPanel::top("bar").show(ctx, |ui| {
                 egui::menu::bar(ui, |ui| {
-                    ui.menu_button("File", |ui| {});
+                    ui.menu_button("File", |ui| {
+                        if ui.button("Load shader..").clicked() {
+                            ui.close_menu();
+
+                            self.path = rfd::FileDialog::new()
+                                .set_title("Select fragment shader")
+                                .pick_file();
+                        }
+
+                        ui.separator();
+
+                        if ui.button("Quit").clicked() {
+                            ui.close_menu();
+
+                            std::process::exit(0);
+                        }
+                    });
                 });
             });
 
             egui::SidePanel::left("tools").show(ctx, |ui| {
                 ui.vertical_centered(|ui| {
                     ui.label("Currently loaded shader:");
-                    ui.monospace(self.path.as_deref().unwrap_or("(none)"));
+                    ui.monospace(
+                        self.path
+                            .as_ref()
+                            .map(|path| path.display().to_string())
+                            .unwrap_or("(none)".into()),
+                    );
 
                     ui.collapsing("Uniforms", |ui| {
                         ui.label("Uniforms value and types provided to the shader.");
